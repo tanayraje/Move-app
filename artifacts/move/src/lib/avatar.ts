c84307880543cddb2613198ba77fe77669051475
tanayraje <tanayraje99@gmail.com>
2026-08-16 17:45:41 +0000

    Update DiceBear avatar style

--- artifacts/move/src/lib/avatar.ts
+++ artifacts/move/src/lib/avatar.ts
@@ -1,5 +1,5 @@
 export const AVATAR_BASE_URL =
-  "https://api.dicebear.com/10.x/notionists-neutral/svg?backgroundColor=69d2e7&paperColor=&inkColor=083a54&eyebrowsVariant=variant01,variant02,variant03,variant04,variant05,variant07,variant08,variant09,variant10,variant11,variant12&mouthVariant=variant01,variant02,variant03,variant04,variant05,variant06,variant08,variant09,variant10,variant11,variant12,variant13,variant14,variant15,variant16,variant17,variant18,variant19,variant20,variant21,variant22,variant23,variant24,variant25,variant26,variant27,variant28,variant29,variant30";
+  "https://api.dicebear.com/10.x/notionists-neutral/svg?eyebrowsVariant=variant01,variant02,variant03,variant04,variant05,variant07,variant08,variant09,variant10,variant11,variant12&eyesVariant=variant01,variant03,variant05&mouthVariant=variant01,variant02,variant03,variant04,variant05,variant06,variant08,variant09,variant10,variant11,variant12,variant13,variant14,variant15,variant16,variant17,variant19,variant20,variant21,variant22,variant23,variant25,variant26,variant27,variant28,variant29,variant30&backgroundColor=69d2e7&inkColor=083a54&paperColor=";
 
 export function getAvatarUrl(seed?: string, size = 128) {
   return `${AVATAR_BASE_URL}&seed=${encodeURIComponent(
